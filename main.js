126cb0c03a7c6103312dc23f6f5b69af61d6c876
Asadi.Ali <you@example.com>
2021-08-13 18:55:17 +0430

    commit 1

---
+++ main.js
@@ -0,0 +1,16 @@
+var input = document.getElementById("name").value;
+console.log(input);
+
+function showMessage() {
+	var name = "hello";
+	var age = 20;
+	console.log(name + age);
+	alert("hello");
+}
+
+console.log("Hello2");
+
+
+function showName() {
+	console.log("Reza");
+}
