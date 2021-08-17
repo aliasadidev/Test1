d7bca7b29f1e32b9770c69bb0850b9234759f2fd
izadpanah <mohammad.saein@gmail.com>
2021-08-17 17:12:17 +0430

    solved tamrin1

---
+++ main.js
@@ -0,0 +1,12 @@
+function on() {
+	document.getElementById("img").src = "img/pic_bulbon.gif"
+}
+
+function off() {
+	document.getElementById("img").src = "img/pic_bulboff.gif";
+}
+
+function onError() {
+	document.getElementById("img").src = "img/no-image.jpg";
+}
+
